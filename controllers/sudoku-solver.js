f6210e530d2a3f2a5e0b46803bd06f5d5020787b
Clark Reyes <clarkenreyes@gmail.com>
2023-08-11 21:28:49 +0800

    feat: finished implementing solver logic

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -6,7 +6,12 @@ class SudokuSolver {
 
   checkRowPlacement(puzzleString, row, column, value) {
     const rowToNum = { a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7, i: 8 };
-    const rowNum = rowToNum[row.toLowerCase()];
+    let rowNum;
+    if (/^[a-iA-I]$/.test(row)) {
+      rowNum = rowToNum[row.toLowerCase()];
+    } else {
+      rowNum = row;
+    }
     const startPosition = rowNum * 9;
 
     return !puzzleString
@@ -24,13 +29,18 @@ class SudokuSolver {
 
   checkRegionPlacement(puzzleString, row, column, value) {
     const rowToNum = { a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7, i: 8 };
-    const rowNum = rowToNum[row.toLowerCase()];
+    let rowNum;
+    if (/^[a-iA-I]$/.test(row)) {
+      rowNum = rowToNum[row.toLowerCase()];
+    } else {
+      rowNum = row;
+    }
     const rowRange = Math.floor(rowNum / 3);
-    const colRange = Math.floor(column / 3);
+    const colRange = Math.floor((column - 1) / 3);
 
     for (let i = colRange * 3; i < colRange * 3 + 3; i++) {
       for (let j = rowRange * 3; j < rowRange * 3 + 3; j++) {
-        if (puzzleString[i * 9 + j] == value) return false;
+        if (puzzleString[j * 9 + i] == value) return false;
       }
     }
     return true;
@@ -38,21 +48,26 @@ class SudokuSolver {
 
   solve(puzzleString) {
     if (!puzzleString.includes(".")) return puzzleString;
+    let dotIndex = puzzleString.indexOf(".");
+    for (let i = 1; i <= 9; i++) {
+      let row = Math.floor(dotIndex / 9);
+      let col = (dotIndex % 9) + 1;
 
-    for (let i = 0; i < 81; i++) {
-      //
+      if (
+        this.checkColPlacement(puzzleString, row, col, i) &&
+        this.checkRowPlacement(puzzleString, row, col, i) &&
+        this.checkRegionPlacement(puzzleString, row, col, i)
+      ) {
+        let newPuzzleString =
+          puzzleString.substring(0, dotIndex) +
+          i +
+          puzzleString.substring(dotIndex + 1);
+        let result = this.solve(newPuzzleString);
+        if (result) return result;
+      }
     }
+    return false;
   }
 }
 
 module.exports = SudokuSolver;
-
-let solver = new SudokuSolver();
-console.log(
-  solver.checkRegionPlacement(
-    "5..91372.3...8.5.9.9.25..8.68.47.23...95..46.7.4.....5.2.......4..8916..85.72...3",
-    "a",
-    2,
-    1
-  )
-);
