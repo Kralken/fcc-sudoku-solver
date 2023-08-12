89c45ac4df34cf0619c30f76b3db03741cefcbfa
Clark Reyes <clarkenreyes@gmail.com>
2023-08-12 20:04:38 +0800

    feat: added handling of POST request to /api/check route

--- routes/api.js
+++ routes/api.js
@@ -12,7 +12,47 @@ class InputError extends Error {
 module.exports = function (app) {
   let solver = new SudokuSolver();
 
-  app.route("/api/check").post((req, res) => {});
+  app.route("/api/check").post((req, res) => {
+    try {
+      const puzzleString = req.body.puzzle;
+      const coordinate = req.body.coordinate;
+      const value = req.body.value;
+
+      if (!puzzleString || !coordinate || !value)
+        throw new InputError("Required field(s) missing");
+      if (!/^[1-9.]+$/.test(puzzleString))
+        throw new InputError("Invalid characters in puzzle");
+      if (!/^.{81}$/.test(puzzleString))
+        throw new InputError("Expected puzzle to be 81 characters long");
+      if (!/^[a-iA-I]{1}[1-9]{1}$/.test(coordinate))
+        throw new InputError("Invalid coordinate");
+      if (!/^[1-9]{1}$/.test(value)) throw new InputError("Invalid value");
+
+      let [row, col] = coordinate.split("");
+      let errors = [];
+      if (!solver.checkRowPlacement(puzzleString, row, col, value))
+        errors.push("row");
+      if (!solver.checkColPlacement(puzzleString, row, col, value))
+        errors.push("column");
+      if (!solver.checkRegionPlacement(puzzleString, row, col, value))
+        errors.push("region");
+
+      if (errors.length == 0) {
+        res.status(200).json({ valid: true });
+      } else {
+        res.status(200).json({
+          valid: false,
+          conflict: errors,
+        });
+      }
+    } catch (e) {
+      if (e instanceof InputError) {
+        res.status(200).json({ error: e.message });
+      } else {
+        res.status(500).send("something went wrong");
+      }
+    }
+  });
 
   app.route("/api/solve").post((req, res) => {
     try {
